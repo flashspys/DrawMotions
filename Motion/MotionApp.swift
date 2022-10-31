0f1740509495c0e56971df22bc3f27c807550ab3
Christian Menschel <christian@tapwork.de>
2022-10-31 08:17:12 +0100

    Initial Commit

---
+++ Motion/MotionApp.swift
@@ -0,0 +1,17 @@
+//
+//  MotionApp.swift
+//  Motion
+//
+//  Created by Christian Menschel on 31.10.22.
+//
+
+import SwiftUI
+
+@main
+struct MotionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
