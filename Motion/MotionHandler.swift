b43ff22c4a87c29faaadc2ac3e6f264456448376
Torsten Kammer <2102135+cochrane@users.noreply.github.com>
2022-11-03 20:50:38 +0100

    Restore MotionHandler to original
    Removed experiments that didn't go anywhere.

--- Motion/MotionHandler.swift
+++ Motion/MotionHandler.swift
@@ -11,26 +11,23 @@ import CoreMotion
 class MotionHandler: ObservableObject {
     let manager = CMMotionManager()
     
-    struct Values: Identifiable {
+    struct Values {
         let x: Double
         let y: Double
         let z: Double
-        
-        let date = Date.timeIntervalSinceReferenceDate
-        let id: Int
     }
     
     static let updateInterval = 5.0
 
-    @Published var acceleration: Values = .init(x: 0, y: 0, z: 0, id: 0)
+    @Published var acceleration: Values = .init(x: 0, y: 0, z: 0)
     @Published var accelerations = [Values]()
-    @Published var rotation: Values = .init(x: 0, y: 0, z: 0, id: 0)
+    @Published var rotation: Values = .init(x: 0, y: 0, z: 0)
     @Published var rotations = [Values]()
-    @Published var magneticField: Values = .init(x: 0, y: 0, z: 0, id: 0)
+    @Published var magneticField: Values = .init(x: 0, y: 0, z: 0)
     @Published var magneticFields = [Values]()
-    @Published var gravity: Values = .init(x: 0, y: 0, z: 0, id: 0)
+    @Published var gravity: Values = .init(x: 0, y: 0, z: 0)
     @Published var gravities = [Values]()
-    @Published var gyro: Values = .init(x: 0, y: 0, z: 0, id: 0)
+    @Published var gyro: Values = .init(x: 0, y: 0, z: 0)
     @Published var gyros = [Values]()
     
     init() {
@@ -52,32 +49,32 @@ class MotionHandler: ObservableObject {
     }
 
     private func readAcceleration( _ values: CMAcceleration) {
-        let acceleration = Values(x: values.x, y: values.y, z: values.z, id: magneticFields.count)
+        let acceleration = Values(x: values.x, y: values.y, z: values.z)
         accelerations.append(acceleration)
         self.acceleration = acceleration
     }
     
     private func readMagneticField(_ values: CMCalibratedMagneticField) {
-        let magneticField = Values(x: values.field.x, y: values.field.x, z: values.field.z, id: magneticFields.count)
+        let magneticField = Values(x: values.field.x, y: values.field.x, z: values.field.z)
         magneticFields.append(magneticField)
         self.magneticField = magneticField
     }
     
     private func readGravity( _ values: CMAcceleration) {
-        let gravity = Values(x: values.x, y: values.y, z: values.z, id: gravities.count)
+        let gravity = Values(x: values.x, y: values.y, z: values.z)
         gravities.append(gravity)
         self.gravity = gravity
     }
     
     private func readRotation(_ values: CMRotationRate) {
-        let rotation = Values(x: values.x, y: values.y, z: values.z, id: rotations.count)
+        let rotation = Values(x: values.x, y: values.y, z: values.z)
         rotations.append(rotation)
         self.rotation = rotation
     }
     
     private func readGyros(_ values: CMGyroData?) {
         guard let values = values?.rotationRate else { return }
-        let gyro = Values(x: values.x, y: values.y, z: values.z, id: gyros.count)
+        let gyro = Values(x: values.x, y: values.y, z: values.z)
         gyros.append(rotation)
         print(gyro)
         self.gyro = gyro
